614dcec8203efb64d2bdc2ae523e13939a253510
xiaohao <19929238507@163.com>
2021-08-12 18:08:18 +0800

    项目框架搭建

---
+++ react-antd-admin-ts/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+
+export default class Login extends Component<any, any>{
+    render() {
+        return (
+         <div>
+             登录页面
+         </div>
+        )
+    }
+}
